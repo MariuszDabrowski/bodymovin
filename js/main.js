fc692641e925ea1578ee9d520e2a94dca3fb4ced
Mariusz Dabrowski <wayrse@gmail.com>
2017-08-20 19:49:25 -0400

    Cloud movement

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 (function() {
 
+  const cludBoundary = document.querySelector('.cloud-boundary');
+  const bike = document.querySelector('.bike');
+  const bmContainer = document.querySelector('#bm');
+
+  // ------------------------
+  // Bodymovin bike animation
+  // ------------------------
+
   var animation = bodymovin.loadAnimation({
-    container: document.querySelector('#bm'),
+    container: bmContainer,
     renderer: 'svg',
     loop: true,
     autoplay: true,
@@ -10,7 +18,11 @@
 
   animation.setSpeed(1);
 
-  var path = anime.path('.test');
+  // ------------------------------
+  // Anime.js animation around path
+  // ------------------------------
+
+  var path = anime.path('.outline');
 
   var motionPath = anime({
     targets: '.bike',
@@ -22,4 +34,82 @@
     loop: true
   });
 
+  // -----------------------------------------------
+  // Detect collision between the bike and the cloud
+  // -----------------------------------------------
+
+  const dot = document.querySelector('.dot');
+
+  const detectCollision = function(item1, item2) {
+    
+
+    return !(
+      item1.right < item2.left ||
+      item1.left > item2.right ||
+      item1.bottom + 40 < item2.top ||
+      item1.top > item2.bottom
+    );
+  };
+
+  // -------------------------------
+  // Keyboard controls for the cloud
+  // -------------------------------
+
+  let cludBoundaryAngle = 0;
+  let cludBoundaryVelocity = 0;
+  let left = false;
+  let right = false;
+  let lastDirection = '';
+
+  document.addEventListener('keydown', (e) => {
+    if(e.keyCode === 37) {
+      left = true;
+      lastDirection = 'left';
+    } else if (e.keyCode === 39) {
+      left = true;
+      lastDirection = 'right';
+    }
+  });
+
+  document.addEventListener('keyup', (e) => {
+    if(e.keyCode === 37) {
+      left = false;
+    } else if (e.keyCode === 39) {
+      left = false;
+    }
+  });
+
+  const step = function() {
+    if(left || right) {
+      if (cludBoundaryVelocity < 2) {
+        cludBoundaryVelocity += 0.2;
+      }
+    }
+
+    cludBoundary.style.transform = `translateX(-50%) rotate(${cludBoundaryAngle}deg)`
+
+    if(cludBoundaryVelocity > 0) {
+      if(lastDirection === 'left') {
+        cludBoundaryAngle -= cludBoundaryVelocity;
+      } else if (lastDirection === 'right') {
+        cludBoundaryAngle += cludBoundaryVelocity;
+      }
+
+      cludBoundaryVelocity -= 0.05;
+    } else {
+      cludBoundaryVelocity = 0;
+    }
+
+    if(detectCollision(bmContainer.getBoundingClientRect(), cludBoundary.getBoundingClientRect())) {
+      if(!bmContainer.classList.contains('active')) {
+        bmContainer.classList.add('active');
+      } else {
+        bmContainer.classList.remove('active');
+      }
+    }
+
+    window.requestAnimationFrame(step);
+  };
+
+  window.requestAnimationFrame(step);
 })();
