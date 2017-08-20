cd6860e3d5fbcbec46ecd7dcb94bee5da1239ac5
Mariusz Dabrowski <wayrse@gmail.com>
2017-08-19 21:42:57 -0400

    Got the biker working

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'json/bike.json',
+    path: 'json/data.json',
   });
+
+  animation.setSpeed(1);
 
   var path = anime.path('.test');
 
@@ -16,7 +18,7 @@
     translateY: path('y'),
     rotate: path('angle'),
     easing: 'linear',
-    duration: 8000,
+    duration: 15000,
     loop: true
   });
 
